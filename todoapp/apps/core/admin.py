2a25eabc92976b91d6369aeaaaa4bfd7a0b72784
Filipe <filipe.rj149@gmail.com>
2020-07-28 18:17:25 -0300

    lesson 73 added
    user registration template added

--- todoapp/apps/core/admin.py
+++ todoapp/apps/core/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 # Register your models here.
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
+#admin.site.unregister(User)
+#admin.site.unregister(Group)
 
 admin.site.site_header = 'TodoApp Login'
 admin.site.site_title = 'TodoApp Admin Panel'

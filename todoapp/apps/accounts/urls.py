56dc949c04bc4f5e92c24b4c3a1f7fa905104509
Filipe <filipe.rj149@gmail.com>
2020-07-28 22:34:30 -0300

    lesson 78-79 added
    password changing system and template added

--- todoapp/apps/accounts/urls.py
+++ todoapp/apps/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('novo-usuario/', views.add_user, name='add_user'),
     path('login/', views.user_login, name='user_login'),
     path('sair/', views.user_logout, name='user_logout'),
+    path('alterar-senha/', views.user_change_password, name='user_change_password'),
 ]

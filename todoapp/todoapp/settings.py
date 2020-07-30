de82d6a2e145456b37485ca8411f643c46dae65b
Filipe <filipe.rj149@gmail.com>
2020-07-29 22:14:34 -0300

    Revert "Revert "Deploy inicial""
    This reverts commit a7094d04a816fc2c00c407cc3427c153b1fd807d.

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import sys 
 from decouple import config
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APPS_DIR = os.path.join(BASE_DIR, 'apps')
@@ -145,12 +146,14 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
